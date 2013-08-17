3ded29adf4a67bc1408e5dddf1154fc7cf2757d7
Aram Zucker-Scharff <azuckers@gmu.edu>
2013-08-17 10:13:18 -0400

    Init setup

---
+++ footer.php
@@ -0,0 +1,39 @@
+<div class="clearfix"></div>
+        <div id="footer">
+        	<?php wp_nav_menu(  array( 'theme_location' => 'footer-nav', 'container_class' => 'table', 'container_id' => '', 'before' => '', 'after' => '', 'menu_class' =>'', 'menu_id' => '' ));  ?>
+        </div>
+        <div class="copyright">
+		<div class="social_icons">
+			<ul>
+				<li class="follow_us">Follow Us</li>
+				<li class="social_icon twitter_icon"><a href="http://twitter.com/SES_Government" target="_blank"></a></li>
+				<li class="social_icon facebook_icon"><a href="http://www.facebook.com/pages/SES-Government-Solutions/515093405169120" target="_blank"></a></li>
+				<li class="social_icon youtube_icon"><a href="http://www.youtube.com/user/SESGovernment?feature=watch" target="_blank"></a></li>
+				<li class="social_icon linkedin_icon"><a href="http://www.linkedin.com/company/ses-government-solutions" target="_blank"></a></li>
+			</ul>
+		</div>
+		Copyright © 2005–2013 SES Government Solutions <a href="/privacy-policy">Privacy Policy</a><a href="/sitemap.xml">Sitemap</a>
+	</div>
+    </div> <!-- END #wrapper -->
+</div> <!-- END #shell -->
+<div class="clearfix"></div>
+<?php wp_footer(); ?>
+<script type="text/javascript">
+ 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25742339-1']);
+  _gaq.push(['_trackPageview']);
+ 
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+ 
+</script>
+<script type="text/javascript" language="javascript">llactid=16037</script> 
+<script type="text/javascript" language="javascript" src="http://t5.trackalyzer.com/trackalyze.js"></script>
+
+
+</body>
+</html>
