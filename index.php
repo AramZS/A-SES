a0e0178d8fb4f07c34e13fe346cd93998375f639
Aram Zucker-Scharff <azuckers@gmu.edu>
2013-08-18 13:33:50 -0400

    Fix for index

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
  *
  * @var bool
  */
-define('WP_USE_THEMES', true);
-
+if (WP_USE_THEMES != true){
+    define('WP_USE_THEMES', true);
+}
 /** Loads the WordPress Environment and Template */
 require('./wp-blog-header.php');
