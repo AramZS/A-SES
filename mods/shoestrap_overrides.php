40e8dca5b8703aeb0cf5f8ed71f3d3bd74839e09
shawn chollette <boywondercreative@gmail.com>
2014-02-11 08:44:34 -0600

    added files from ses_v2

--- mods/shoestrap_overrides.php
+++ mods/shoestrap_overrides.php
@@ -39,31 +39,39 @@ add_action( 'shoestrap_before_main', 'tbwa_before_main' );
 function tbwa_below_top_navbar() {
 if (is_front_page()) {
 	get_template_part('parts/hero');
-}}
+}
 
+get_template_part('parts/mobilenav');
+
+}
 add_action( 'shoestrap_below_top_navbar', 'tbwa_below_top_navbar' );
 
 
 
-
-
-
-
 function my_custom_pre_wrap() { ?>
+
 <div class="page-wrap container">
 <div class="row row-offcanvas row-offcanvas-left">
-  
+
 
 <?php get_template_part('parts/sidenav');?>
 
 
 <div class="page-body col-xs-12 col-sm-9 col-md-9">
-    <p class="pull-left visible-xs">
-    <button data-toggle="offcanvas" class="btn btn-primary btn-xs" type="button">Toggle nav</button>
-    </p>  
+
+<?php
+global $post;
+if ( !is_front_page() ) {
+echo get_template_part('parts/togglenav');
+}
+?>
+
+
+     
 <?php
 }
 add_action( 'shoestrap_pre_wrap', 'my_custom_pre_wrap' );
+
 //*
 function my_custom_after_wrap() {
   echo '';
