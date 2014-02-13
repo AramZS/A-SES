8e40e52ccee3d9ff510ebf124d1ab554f1283eca
shawn chollette <boywondercreative@gmail.com>
2014-02-13 04:13:17 -0600

    add thumbnail size to hero

--- templates/hero.php
+++ templates/hero.php
@@ -2,8 +2,8 @@
 <div class="hero">
     <div class="container">
         <div class="post-thumb">
-            <?php //the_post_thumbnail(9999,9999);?>
-            <?php the_post_thumbnail();?>
+            <?php the_post_thumbnail(9999,9999);?>
+            <?php //the_post_thumbnail();?>
         </div>
     </div> 
 </div>
