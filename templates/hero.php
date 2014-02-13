b6bcb2a63aec430e9a32dbf9a394f428ab0bfbcd
shawn chollette <boywondercreative@gmail.com>
2014-02-13 04:10:45 -0600

    move hero to /templates

---
+++ templates/hero.php
@@ -0,0 +1,9 @@
+<?php //if (is_front_page()) { ?>
+<div class="hero">
+    <div class="container">
+        <div class="post-thumb">
+            <?php //the_post_thumbnail(9999,9999);?>
+            <?php the_post_thumbnail();?>
+        </div>
+    </div> 
+</div>
