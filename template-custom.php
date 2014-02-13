869aa9fce1e43fd3b3fa4d761a55c87f42676410
shawn chollette <boywondercreative@gmail.com>
2014-02-12 23:18:20 -0600

    update template-custom

--- template-custom.php
+++ template-custom.php
@@ -3,13 +3,5 @@
 Template Name: Custom Template
 */
 
-if ( !has_action( 'shoestrap_page_header_override' ) )
-  get_template_part('templates/page', 'header');
-else
-  do_action( 'shoestrap_page_header_override' );
-
-if ( !has_action( 'shoestrap_content_page_override' ) )
   get_template_part('templates/content', 'page');
-  get_template_part('parts/tabber');
-else
-  do_action( 'shoestrap_content_page_override' );
+  get_template_part('parts/tabber_2');
