866a688b5d2efc33f15779dca15e9a7ccca8b264
shawn chollette <boywondercreative@gmail.com>
2014-02-13 15:57:02 -0600

    fix thumbnail dimensions

--- templates/add_hero.php
+++ templates/add_hero.php
@@ -2,8 +2,7 @@
 <div class="hero">
     <div class="container">
         <div class="post-thumb">
-            <?php //the_post_thumbnail(9999,9999);?>
-            <?php the_post_thumbnail();?>
+            <?php the_post_thumbnail(9999,9999);?>
         </div>
     </div> 
 </div>
