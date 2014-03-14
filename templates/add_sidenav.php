b2beeec584c56cbc9df1d4df13a8d984a58ee4a9
shawn chollette <boywondercreative@gmail.com>
2014-03-14 15:19:19 -0500

    syncing with webenable

--- templates/add_sidenav.php
+++ templates/add_sidenav.php
@@ -28,52 +28,32 @@
           </ul>
 
         <?php if ( is_page() ) { ?>
-        <?php
-        if($post->post_parent)
-        $children = wp_list_pages('title_li=&child_of='.$post->post_parent.'&echo=0'); else
-        $children = wp_list_pages('title_li=&child_of='.$post->ID.'&echo=0');
-        if ($children) { ?>
-        
-        <div class="title"><?php $parent_title = get_the_title($post->post_parent); echo $parent_title; ?><span></span></div>
-        <ul class="nav navmenu-nav">
-            <?php echo $children; ?>
-        </ul>
-        <?php } } ?>    
+
+        <div class="nav-collapse collapse">
+          <ul class="nav">
+
+              <?php wp_list_pages(array('title_li' => '', 'exclude' => 4)); ?>
+
+          </ul>
+        </div><!--/.nav-collapse -->
+    <?php }  ?>    
     </nav>
 
     
-    <div id="side-nav-holder" class="clearfix hidden-xs">
-    
+<?php /*?>    <div id="side-nav-holder" class="clearfix hidden-xs">
         <!--this is a menu -->
         <nav id="side-nav" class="side-nav  hidden-xs" role="navigation">
           <?php if (has_nav_menu('sidenav')) :
               wp_nav_menu( array( 'theme_location' => 'sidenav', 'menu_class' => 'list-group' ) );
             endif;?>
         </nav>
-        
         <!--this is a sidebar   --> 
         <div class="sidebar hidden-xs">
             <?php if ( is_active_sidebar( 'left-sidebar' )) : ?>
                 <?php dynamic_sidebar('left-sidebar'); ?>
             <?php endif; ?>
         </div>
-
-        <!--just in case we end up needing to use a custom nav walker-->
-        <nav id="myNavmenu" class="navmenu navmenu-default navmenu-fixed-left offcanvas" role="navigation">
-            <?php if ( is_page() ) { ?>
-            <?php
-            if($post->post_parent)
-            $children = wp_list_pages('title_li=&child_of='.$post->post_parent.'&echo=0'); else
-            $children = wp_list_pages('title_li=&child_of='.$post->ID.'&echo=0');
-            if ($children) { ?>
-            
-            <div class="title"><?php $parent_title = get_the_title($post->post_parent); echo $parent_title; ?><span></span></div>
-            <ul class="nav navmenu-nav">
-                <?php echo $children; ?>
-            </ul>
-            <?php } } ?>    
-        </nav>
-
-
-    </div> 
+    </div> <?php */?>
 <?php } ?>
+
+
