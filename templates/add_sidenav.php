1cdb8277e1912ee68290959f4ccd203c6baa1d56
shawn chollette <boywondercreative@gmail.com>
2014-03-18 14:53:09 -0500

    remove _HOLDS

--- templates/add_sidenav.php
+++ templates/add_sidenav.php
@@ -1,24 +1,11 @@
 <?php if (!is_front_page()){
-//
 	if ( is_page() ) {
 		if ( function_exists( 'has_nav_menu' ) && has_nav_menu( 'primary_navigation' ) ) { ?>
             <div class="side-nav-wrap clearfix hidden-xs">
                 <nav class="side-nav-lg">
                     <?php wp_nav_menu( array(
                         'theme_location'  => 'primary_navigation',
-//                        'menu'            => '',
-//                        'container'       => '',
-//                        'container_class' => '',
-//                        'container_id'    => '',
                         'menu_class'      => 'list-group',
-//                        'menu_id'         => '',
-//                        'echo'            => true,
-//                        'fallback_cb'     => 'wp_page_menu',
-//                        'before'          => '',
-//                        'after'           => '',
-//                        'link_before'     => '',
-//                        'link_after'      => '',
-//                        'items_wrap'      => '',
                         'depth'           => 2,
                         'sub_menu'		  => true,
                         'walker'          => new clean_walker()
@@ -34,11 +21,11 @@
                 </aside>
             </div> 
          <?php endif; ?>
-         
 <div class="clearfix visible-xs">
     <nav class=" side-nav-sm navmenu navmenu-default navmenu-fixed-left offcanvas" role="navigation">
-<a id="nav-close" data-canvas=".canvas" data-target=".navmenu" data-recalc="false" data-toggle="offcanvas" class="navbar-toggle el-icon-remove-sign"></a>
-
+        <div class="nav-close-clear clearfix">
+        <a id="nav-close" data-canvas=".canvas" data-target=".navmenu" data-recalc="false" data-toggle="offcanvas" class="navbar-toggle el-icon-remove-sign"></a>
+        </div>
         <?php wp_nav_menu( array(
             'theme_location'  => 'primary_navigation',
             'menu'            => '',
@@ -56,7 +43,6 @@
             'items_wrap'      => '',
             'depth'           => 2,
             'sub_menu'		  => true
-//			'walker'          => new clean_walker()
         ));?>
     </nav>
 </div>    
