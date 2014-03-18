60400f7292d34638a4ca0f8c896c8b306995ccff
shawn chollette <boywondercreative@gmail.com>
2014-03-18 18:19:36 -0500

    remove refuse

--- mods/scrollup/scrollUp.js
+++ mods/scrollup/scrollUp.js
@@ -1,17 +1,17 @@
 jQuery(document).ready(function() {
 	var offset = 220;
 	var duration = 500;
-	jQuery(window).scroll(function() {
-		if (jQuery(this).scrollTop() > offset) {
-			jQuery('.scrollup').fadeIn(duration);
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > offset) {
+			$('.scrollup').fadeIn(duration);
 		} else {
-			jQuery('.scrollup').fadeOut(duration);
+			$('.scrollup').fadeOut(duration);
 		}
 	});
 
-	jQuery('.scrollup').click(function(event) {
+	$('.scrollup').click(function(event) {
 		event.preventDefault();
-		jQuery('html, body').animate({scrollTop: 0}, duration);
+		$('html, body').animate({scrollTop: 0}, duration);
 		return false;
 	})
 });
